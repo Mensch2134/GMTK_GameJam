86be2faf0e86f6238d488ee4ddb283f46e4cc7e2
MaxMiehe <miehemax@uni-bremen.de>
2021-06-13 13:04:56 +0200

    SoulLink besser

--- Assets/Scripts/SoulConnection.cs
+++ Assets/Scripts/SoulConnection.cs
@@ -9,6 +9,19 @@ public class SoulConnection : MonoBehaviour
     public Transform player0Pos;
     public Transform player1Pos;
 
+    [SerializeField]
+    private Texture[] textures;
+
+    [SerializeField]
+    private Texture dragTexture;
+
+    private int animationStep;
+
+    [SerializeField]
+    private float fps = 30f;
+
+    private float fpsCounter;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +33,27 @@ public class SoulConnection : MonoBehaviour
     {
         connection.SetPosition(0, player0Pos.position);
         connection.SetPosition(1, player1Pos.position);
+
+        if (GameManager.DragPhase)
+        {
+            fpsCounter = 0f;
+            animationStep = 0;
+            connection.material.SetTexture("_MainTex", dragTexture);
+        } else
+        {
+            fpsCounter += Time.deltaTime;
+            if (fpsCounter >= 1f / fps)
+            {
+                animationStep++;
+                if (animationStep == textures.Length)
+                {
+                    animationStep = 0;
+                }
+
+                connection.material.SetTexture("_MainTex", textures[animationStep]);
+
+                fpsCounter = 0f;
+            }
+        }       
     }
 }
